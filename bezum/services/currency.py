4f6fefa3f6f5fa9fb55d921957d7462eaa6062e3
andrax <andronov_maksimka@mail.ru>
2025-04-01 18:47:55 +0300

    updated currencies to schema

--- bezum/services/currency.py
+++ bezum/services/currency.py
@@ -4,11 +4,15 @@ import asyncio
 import random
 import json 
 from bezum.db.schemas.currency import CurrencySchema
+from bezum.utils.singleton import SingletonMeta
 
-class CurrencyService:
-    async def get_all_currencies(self) -> CurrencySchema:
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB,THBRUB,RSDRUB,JPYRUB,LKRRUB,MDLRUB,MMKRUB,GELRUB,GBPRUB,BYNRUB,CADRUB,CHFRUB,CNYRUB,ETHRUB,BCHRUB,BTCRUB,BYNRUB&key=d0f096c77d6302528ed02df44ef86f93') as response:
+class CurrencyService(metaclass=SingletonMeta):
+    
+
+    async def get_all_currencies(self):
+        async with aiohttp.ClientSession() as self.session:
+            response = await self.session.get('https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB,THBRUB,RSDRUB,JPYRUB,LKRRUB,MDLRUB,MMKRUB,GELRUB,GBPRUB,BYNRUB,CADRUB,CHFRUB,CNYRUB,ETHRUB,BCHRUB,BTCRUB,BYNRUB&key=d0f096c77d6302528ed02df44ef86f93')
+            async with response:
                 if response.status == 200:
                     data_bait = await response.read()
                     data_json = json.loads(data_bait.decode('utf-8'))
@@ -22,13 +26,13 @@ class CurrencyService:
                     return list_data
                 else:
                     raise Exception(f'Error fetching data: {response.status}')
-    
-    
+
     async def get_random_currency(self):
-        currencies = 'USDRUB','EURRUB','THBRUB','RSDRUB','JPYRUB','LKRRUB','MDLRUB','MMKRUB','GELRUB','GBPRUB','BYNRUB','CADRUB','CHFRUB','CNYRUB','ETHRUB','BCHRUB','BTCRUB','BYNRUB'
-        rand_cur = random.choice(currencies)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://currate.ru/api/?get=rates&pairs={rand_cur}&key=d0f096c77d6302528ed02df44ef86f93') as response:
+        async with aiohttp.ClientSession() as self.session:
+            currencies = 'USDRUB','EURRUB','THBRUB','RSDRUB','JPYRUB','LKRRUB','MDLRUB','MMKRUB','GELRUB','GBPRUB','BYNRUB','CADRUB','CHFRUB','CNYRUB','ETHRUB','BCHRUB','BTCRUB','BYNRUB'
+            rand_cur = random.choice(currencies)
+            response = await self.session.get(f'https://currate.ru/api/?get=rates&pairs={rand_cur}&key=d0f096c77d6302528ed02df44ef86f93')
+            async with response:
                 if response.status == 200:
                     data_bait = await response.read()
                     data_json = json.loads(data_bait.decode('utf-8'))
@@ -36,8 +40,16 @@ class CurrencyService:
                     data = data_json['data']
                     #print(data, type(data))
                     res = CurrencySchema(key=rand_cur, value=data[rand_cur])
-                    return data
+                    return res
                 else:
                     raise Exception(f'Error fetching data: {response.status}')
-                
-                
+
+
+async def main():
+    currency_service = CurrencyService()
+    await currency_service.get_all_currencies()
+    await currency_service.get_random_currency()
+    await currency_service.close_session()
+
+if __name__ == "__main__":
+    asyncio.run(main())
